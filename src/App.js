e935ec92abe767224e1980c148f9c8f77cbc218b
perry <wupeng6326@163.com>
2024-03-25 16:21:40 +0800

    测试文件初始化

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+// #!/usr/bin/env node
+// module.exports = {};
+
+
+import './App.css';
+import React, { useRef } from "react";
+
+// const SPADesigner = window.mybricks.SPADesigner;
+function App() {
+
+// console.log(SPADesigner);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
